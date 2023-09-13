2c72aa996f6fc10513bc4bc4fee21f7ccd19d5ec
Nitin Budhlakoti <nitinbdkt777@gmail.com>
2023-09-13 19:37:47 +0530

    dynamic variable in python
    In this we learn how to declare variables of  string integer . And also learn ways to declare multiple variables in single line..

---
+++ variables.py
@@ -0,0 +1,15 @@
+name='Nitin'
+print(name)
+age=20
+print(age)
+name=21
+# dynamic binding
+print(name)
+
+#declaring multiple variable in single line
+a=4;b=6;c=7
+print(a,b,c)
+
+#another way to declaring variable in single line
+a,b,c=4,5,6
+print(a,b,c)
