b62d877b8ab3f041d9568687d10ebc2f7ed427ed
Nitin Budhlakoti <nitinbdkt777@gmail.com>
2023-09-13 19:25:33 +0530

    python data types and container
    In this here we cover two data types
    1- simple data types
    -> integer,float,boolean,complex,string.
    2-container
    -> list,tuple,set,dictionary

---
+++ data_types.py
@@ -0,0 +1,36 @@
+#integer 
+print(4)
+print(23253256532356532565323)
+print(1e305)
+print(1e310)
+
+# float
+print(4.1)
+print(1.7e307)
+print(1.7e310)
+
+#boolean 
+print(True)
+print(False)
+
+#complex
+print(4+5j)
+
+#string
+print('string')
+print("string double quotes")
+print("""
+string triple quotes
+      """)
+
+#list
+print([1,2,4,5])
+
+#tuple
+print((1,2,3,4))
+
+#set
+print({1,2,3,3,3,4})
+
+#dictionary
+print({'name':"nitin","gender":"male"})
